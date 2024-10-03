cc33b78b276fd561c02e8b80710dcdbb6c8fafdb
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-10-03 17:54:14 -0300

    Update AdoDapper.cs
    Implement interface

--- HospedApp.Dapper/AdoDapper.cs
+++ HospedApp.Dapper/AdoDapper.cs
@@ -1,14 +1,29 @@
 ﻿using System.Data;
 using Dapper;
+using HospedApp.Core;
+using HospedApp.Core.Entities;
 using MySqlConnector;
 namespace HospedApp.Dapper;
 
-public class AdoDapper
+public class AdoDapper : IAdo
 {
     private readonly IDbConnection _conexion;
     //Este constructor usa por defecto la cadena para un conector MySQL
     public AdoDapper(string cadena) => _conexion = new MySqlConnection(cadena);
     public AdoDapper(IDbConnection conexion) => this._conexion = conexion;
 
+    #region 'Hotel'
 
+    private readonly string _queryHotel
+        = @"SELECT * FROM Hotel";
+    public List<Hotel> GetHotel()
+        => _conexion.Query<Hotel>(_queryHotel).ToList();
+
+    public void CreateHotel(Hotel hotel)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    #endregion
 }
