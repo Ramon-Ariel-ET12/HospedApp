e905a1aca3a6915ef84568e18898e44211b7bbb6
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-10-03 17:53:16 -0300

    Interface

--- HospedApp.Core/IAdo.cs
+++ HospedApp.Core/IAdo.cs
@@ -1,6 +1,13 @@
-﻿namespace HospedApp.Core;
+﻿using HospedApp.Core.Entities;
+
+namespace HospedApp.Core;
 
 public interface IAdo
 {
-    
+    #region 'Hotel'
+
+    List<Hotel> GetHotel();
+
+    void CreateHotel(Hotel hotel);
+    #endregion
 }
