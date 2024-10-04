cd96b2887c0495da0b8c33eef0aa3f17a9469c03
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-10-04 01:02:04 -0300

    Update IAdo.cs
    Implementación de interfaz

--- HospedApp.Core/IAdo.cs
+++ HospedApp.Core/IAdo.cs
@@ -4,10 +4,16 @@ namespace HospedApp.Core;
 
 public interface IAdo
 {
+
     #region 'Hotel'
-
-    List<Hotel> GetHotel();
-
+    List<Hotel> GetHotels();
     void CreateHotel(Hotel hotel);
+    void DeleteHotel(int? IdHotel);
+    #endregion
+    /*********************************************/
+    #region 'Client'
+    List<Client> GetClients();
+    void CreateClient(Client client);
+    void DeleteClient(int? IdClient);
     #endregion
 }
