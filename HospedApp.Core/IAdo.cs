7c9edcdf76056d6fe2483b0b3a60d5f7543bb786
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-10-05 02:02:36 -0300

    Update IAdo.cs
    Se agregó más metodos

--- HospedApp.Core/IAdo.cs
+++ HospedApp.Core/IAdo.cs
@@ -1,19 +1,67 @@
 ﻿using HospedApp.Core.Entities;
+using HospedApp.Core.Entities.Relations;
 
 namespace HospedApp.Core;
 
 public interface IAdo
 {
+    #region 'Client'
+    List<Client> GetClients();
+    void CreateClient(Client client);
+    void DeleteClient(int IdClient);
+    #endregion
+
+
+    #region 'User'
+    User? Login(string email, string password);
+    #endregion
+
+
+    #region 'Bed'
+    List<Bed> GetBeds();
+    void CreateBed(Bed bed);
+    void DeleteBed(int IdBed);
+    #endregion
+
+
+    #region 'Room'
+    List<Room> GetRooms();
+    void CreateRoom(Room room);
+    void DeleteRoom(int IdRoom);
+    #endregion
 
     #region 'Hotel'
     List<Hotel> GetHotels();
     void CreateHotel(Hotel hotel);
-    void DeleteHotel(int? IdHotel);
+    void DeleteHotel(int IdHotel);
     #endregion
-    /*********************************************/
-    #region 'Client'
-    List<Client> GetClients();
-    void CreateClient(Client client);
-    void DeleteClient(int? IdClient);
+
+
+    #region 'RoomBed'
+    List<RoomBed> GetRoomBeds();
+    void CreateRoomBed(RoomBed roomBed);
+    void DeleteRoomBed(int IdRoom, int IdBed);
     #endregion
+
+
+    #region 'Address'
+    List<Address> GetAddresses();
+    void CreateAddress(Address address);
+    void DeleteAddress(int IdAddress);
+    #endregion
+
+
+    #region 'HotelRoom'
+    List<HotelRoom> GetHotelRooms();
+    void CreateHotelRoom(HotelRoom hotelRoom);
+    void DeleteHotelRoom(int RoomNumber);
+    #endregion
+
+
+    #region 'Reservation'
+    List<Reservation> GetReservations();
+    void CreateReservation(Reservation reservation);
+    void CancelReservation(int IdReservation);
+    #endregion
+
 }
