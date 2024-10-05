ff2561e8ab24fe48cc9eda33a7cada99816ae9f8
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-10-05 02:15:10 -0300

    Create Trigger.sql
    Agregando numero de cuarto

---
+++ MySQL/HotelRoom/Trigger.sql
@@ -0,0 +1,13 @@
+use hospedapp;
+
+DELIMITER $$
+
+CREATE TRIGGER BefInsHotelRoom BEFORE INSERT ON HotelRoom
+FOR EACH ROW
+BEGIN
+	DECLARE sum int;
+	SELECT COUNT(IdRoom) INTO sum
+	FROM HotelRoom WHERE IdHotel = NEW.IdHotel;
+	SET sum = sum + 1;
+	SET NEW.Number = sum;
+END $$
