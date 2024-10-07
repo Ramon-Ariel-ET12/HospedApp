49939a2aef54c4bb558d4f99fea079b1b180d986
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-10-07 18:08:16 -0300

    Login implementado, faltaria aplicar jwt

--- HospedApp.MVC/Controllers/HomeController.cs
+++ HospedApp.MVC/Controllers/HomeController.cs
@@ -1,27 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using HospedApp.MVC.Models;
+using HospedApp.Core;
 
 namespace HospedApp.MVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IAdo Ado;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IAdo ado)
     {
+        Ado = ado;
         _logger = logger;
     }
+        public IActionResult Index() => View();
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await Ado.Login(email, password);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            return Redirect("/Client");
+        }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ HospedApp.MVC/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using HospedApp.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospedApp.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+
+    }
+}
