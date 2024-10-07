d3d534fd8b08b5cd052e2dbc18a87e42360a0009
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-10-07 16:19:41 -0300

    Se aplicarion unas correciones de mayusuculas, el punto y coma genera errores en el mysql, el use HospedApp es inecesario

--- HospedApp.Test/AdoTest.cs
+++ HospedApp.Test/AdoTest.cs
@@ -6,7 +6,7 @@ namespace HospedApp.Test;
 public class AdoTest
 {
     protected readonly IAdo Ado;
-    private const string _cadena = "Server=localhost;Database=HospedApp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
+    private const string _cadena = "Server=localhost;Database=5to_HospedApp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
     public AdoTest() => Ado = new AdoDapper(_cadena);
     public AdoTest(string cadena) => Ado = new AdoDapper(cadena);
 }
